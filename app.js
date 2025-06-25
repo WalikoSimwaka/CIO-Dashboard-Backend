485ef33271e149ffcb17971068f62bee631799c0
walikosimwaka <waliko.simwaka@nbs.mw>
2025-06-25 04:40:44 +0200

    final2

--- app.js
+++ app.js
@@ -27,6 +27,7 @@ app.get("/test-db", async (req, res) => {
 	}
 });
 
+// Routes
 const priorityTasksRouter = require("./routes/priorityTasks");
 const highPriorityProjectsRouter = require("./routes/highPriorityProjects");
 const warRoomRouter = require("./routes/warRoom");
@@ -35,16 +36,24 @@ app.use("/api/priority-tasks", priorityTasksRouter);
 app.use("/api/high-priority-projects", highPriorityProjectsRouter);
 app.use("/api/war-room", warRoomRouter);
 
+// Global error handler
 app.use((err, req, res, next) => {
 	console.error(err.stack);
 	res.status(500).json({ error: "Something went wrong!" });
 });
 
-const PORT = process.env.PORT || 5000;
+// Start server only if PORT is defined by Railway
+const PORT = process.env.PORT;
+
+if (!PORT) {
+	throw new Error("PORT environment variable is not defined");
+}
+
 const server = app.listen(PORT, () => {
 	console.log(`Server running on port ${PORT}`);
 });
 
+// Graceful shutdown
 process.on("SIGINT", async () => {
 	await closeConnection();
 	server.close(() => {
